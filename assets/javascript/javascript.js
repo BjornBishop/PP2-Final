bc8af6a10334daa2fc1e53ea5c20e3ac2ff07921
Bjorn Bishop <bjornbishop@gmail.com>
2024-06-29 14:12:59 +0000

    initial structure for html, javascript and css created

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,21 @@
+// Now lets plan out the javascript infrastructure 
+
+//global javascript code 
+document.addEventListener("DOMContentLoaded", function() {
+    let controlButtons = document.getElementsByClassName("control-button")
+
+    for (let conButton of controlButtons) {
+        conButton.addEventListener('click', function() {
+            if (this.getAttribute("data-type") === "higher") {
+                alert(`On a High?`);
+                let gameType = this.getAttribute("data-type");
+                runGame();
+            } else {
+                alert(`Feeling low?`);
+                runGame();
+            }
+        })
+    }
+    runGame();
+
+})
